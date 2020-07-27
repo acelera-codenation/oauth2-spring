0db9109a635cd87422246033a1d1f88a524703cf
Samuel Santos <>
2020-07-27 15:54:52 -0300

    Clean unused imports

--- src/main/java/com/challenge/config/CustomWebSecurityAdapter.java
+++ src/main/java/com/challenge/config/CustomWebSecurityAdapter.java
@@ -5,20 +5,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
+@EnableWebSecurity
 @EnableResourceServer
 @EnableAuthorizationServer
-@EnableWebSecurity
 public class CustomWebSecurityAdapter extends WebSecurityConfigurerAdapter {
 
     @Autowired
@@ -38,7 +36,7 @@ public class CustomWebSecurityAdapter extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(this.userService).passwordEncoder(passwordEncoder());
+        auth.userDetailsService(userService).passwordEncoder(passwordEncoder());
     }
 
     @Bean
